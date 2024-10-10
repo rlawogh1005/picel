99f31156fa065bcb9242f2ace80fbccca7f1ca60
cj <ckswls1019@naver.com>
2024-10-10 20:47:14 +0900

    화면오류해결

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// utils/theme.js
+export const toggleTheme = () => {
+  // 라이트 및 다크 테마 간 전환하는 코드
+};
