8e75dcf624ceebc9fb63e4a96e63fbb99dd16070
jhk <qwertyjhk11@naver.com>
2024-10-10 16:37:42 +0900

    추가: MainPage 추가

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/main-style.css';
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <h1>Welcome to the Main Page</h1>
+      {/* TODO: Implement main page content here */}
+    </div>
+  );
+}
+
+export default MainPage;
