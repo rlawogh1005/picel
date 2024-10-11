5d5c30397d643034b7dc53e393db7f65bf7d89a7
jhk <qwertyjhk11@naver.com>
2024-10-12 02:27:38 +0900

    수정: js에서 jsx로 변경

---
+++ src/components/LogoutPopup.jsx
@@ -0,0 +1,21 @@
+// src/components/LogoutPopup.jsx
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/login-style.css';
+
+const LogoutPopup = ({ onConfirm, onCancel }) => (
+  <div className="popup">
+    <div className="popup-content">
+      <p>Do you want to log out?</p>
+      <button onClick={onConfirm} className="btn main-btn">Log Out</button>
+      <button onClick={onCancel} className="btn sub-btn">Cancel</button>
+    </div>
+  </div>
+);
+
+LogoutPopup.propTypes = {
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default LogoutPopup;
---
+++ src/components/NoticePopup.jsx
@@ -0,0 +1,13 @@
+// src/components/NoticePopup.jsx
+import React from 'react';
+
+const NoticePopup = () => (
+  <div className="notice-popup">
+    <ul>
+      <li><img alt="Chat" className="icon chat-icon" />John Doe</li>
+      <li><img alt="Friend Request" className="icon friend-icon" />Jane Smith</li>
+    </ul>
+  </div>
+);
+
+export default NoticePopup;
